88b322b0f02f6efa484783cd53fabc8dd79c19cd
oscarBustos <oscaribh2011@gmail.com>
2024-04-07 14:38:37 +0200

    Dissolving objects was integrated with controls, it is configurable the time it takes to disolve.

--- Assets/TEMP_TO_DELETE/Scripts/CrystalDissolveControl.cs
+++ Assets/TEMP_TO_DELETE/Scripts/CrystalDissolveControl.cs
@@ -1,27 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CrystalDissolveControl : MonoBehaviour
 {
-    [SerializeField] Material mat;
+    [SerializeField] Material material;
+    [SerializeField] float effectTime;
+    [SerializeField] float appearingRatio = 0.1f;
+    [SerializeField] float speed = 2;
 
-    float total = 1;
+    private float total = 1;
     private Material copyMaterial;
+    private bool hidden = true;
+    private float currentTime;
+    private bool applyEffect;
 
     void Start()
     {
-        copyMaterial = new Material(mat);
+        copyMaterial = new Material(material);
         GetComponent<Renderer>().material = copyMaterial;
+        copyMaterial.SetFloat("_Dissolve", total);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
+        
+        if (Input.GetKeyDown(KeyCode.L) && !applyEffect)
         {
-            total -= 0.1f;
-            copyMaterial.SetFloat("_Dissolve", total);
+            ActivateAppearingAndDisappearingEffect();
+        }
+        if (applyEffect && currentTime <= effectTime)
+        {
+            
+            currentTime += Time.deltaTime;
+            float amount = 0;
+            if (hidden)
+                amount = currentTime / effectTime;
+            else
+                amount -= currentTime / effectTime;
+
+            copyMaterial.SetFloat("_Dissolve", total + amount);
+        } 
+        else
+        {
+            applyEffect = false;
+            currentTime = 0;
+            if (hidden)
+            {
+                total = 1;
+            } else
+            {
+                total = 0;
+            }
         }
     }
+
+    #region Coroutines
+
+    private void ActivateAppearingAndDisappearingEffect()
+    {
+        hidden = !hidden;
+        applyEffect = true;
+        
+        // Activbate or deactivate colliders
+    }
+    #endregion
 }
