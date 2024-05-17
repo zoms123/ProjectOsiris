3530a06e1736c220f766b4f3f2a43a7fd2f41e9e
oscarBustos <oscaribh2011@gmail.com>
2024-05-17 15:13:07 +0200

    Adjustments on crystal interaction power, aprearing movable cube and bridges (Platforms). This ones, can be configured to either appear and disappear by player actions, not disapear at all, or disappear by timer

--- Assets/Scripts/Characters/Player/Powers/Crystal/CrystalDissolveEffect.cs
+++ Assets/Scripts/Characters/Player/Powers/Crystal/CrystalDissolveEffect.cs
@@ -1,18 +1,29 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class CrystalDissolveEffect : MonoBehaviour, IInteractable
 {
-    [SerializeField] Material material;
-    [SerializeField] float effectTime;
-    [SerializeField] Collider colliderToDisolve;
-    [SerializeField] Renderer rendererToDisolve;
+    [SerializeField] private Material material;
+    [SerializeField] private float effectTime;
+    [SerializeField] private Collider colliderToDisolve;
+    [SerializeField] private Renderer rendererToDisolve;
+    [SerializeField] private bool ReapearWhenDisolved;
+    [SerializeField] private bool canBeDisolved;
+
+    [Header("Timer")]
+    [SerializeField] private bool activateTimer;
+    [SerializeField] private float activeTime;
  
     private float total = 1;
     private Material copyMaterial;
     private bool hidden = true;
     private float currentTime;
     private bool applyEffect;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private Transform transformToDissolve;
+    private bool timerActivated;
 
     void Start()
     {
@@ -20,6 +31,9 @@ public class CrystalDissolveEffect : MonoBehaviour, IInteractable
         rendererToDisolve.material = copyMaterial;
         copyMaterial.SetFloat("_Dissolve", total);
         colliderToDisolve.enabled = !hidden;
+        transformToDissolve = rendererToDisolve.transform;
+        originalPosition = transformToDissolve.position;
+        originalRotation = transform.rotation;
     }
 
     void Update()
@@ -35,10 +49,33 @@ public class CrystalDissolveEffect : MonoBehaviour, IInteractable
             applyEffect = false;
             currentTime = 0;
             total = hidden ? 1 : 0;
-            colliderToDisolve.enabled = !hidden;            
+            colliderToDisolve.enabled = !hidden;
+            if (hidden && transformToDissolve.position != originalPosition)
+            {
+                Debug.Log("returning to origin " + transformToDissolve.position + " " + originalPosition);
+                transformToDissolve.parent = transform;
+                transformToDissolve.position = originalPosition;
+                transformToDissolve.rotation = originalRotation;
+                if (ReapearWhenDisolved)
+                {
+                    ActivateAppearingAndDisappearingEffect();
+                }
+            } else if(!hidden && activateTimer)
+            {
+                timerActivated = true;
+                StartCoroutine(Timer());
+                
+            }
         }
     }
 
+    private IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(activeTime);
+        timerActivated = false;
+        ActivateAppearingAndDisappearingEffect();
+        
+    }
 
     private float CalculateEffectRatio()
     {
@@ -49,9 +86,16 @@ public class CrystalDissolveEffect : MonoBehaviour, IInteractable
 
     private void ActivateAppearingAndDisappearingEffect()
     {
+        if ((!hidden && transformToDissolve.parent == transform && ReapearWhenDisolved) ||
+            (!hidden && !canBeDisolved && !activateTimer) ||
+            (activateTimer && timerActivated)
+            )
+        {
+            return;
+        }
+            
         hidden = !hidden;
-        applyEffect = true;
-        
+        applyEffect = true;   
     }
 
     public bool CanInteract(PowerType powerType)
