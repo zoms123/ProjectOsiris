c99af6ef89d69f004e4bea0f1eb06edd9fd7494f
campiins <mcampins28@gmail.com>
2024-06-26 19:59:23 +0200

    Canvas scaled to FullHD and modified NoteController
    Merged NoteCanvas with main Canvas

--- Assets/Scripts/UI/NoteController.cs
+++ Assets/Scripts/UI/NoteController.cs
@@ -3,42 +3,54 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class NoteController : MonoBehaviour
+public class NoteController : MonoBehaviour, IInteractable
 {
 	[SerializeField][TextArea] private string noteText;
-	[SerializeField] private GameObject noteCanvas;
+	[SerializeField] private GameObject noteUI;
 	[SerializeField] private TMP_Text noteTextUi;
 
 	private bool playerInRange = false;
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (playerInRange)
-		{
-			if (Input.GetKeyDown(KeyCode.E))
-			{
-				//Open Note
-				noteTextUi.text = noteText;
-				noteCanvas.SetActive(true);
-			}
-		}
+    public bool CanInteract(PowerType powerType)
+    {
+		return playerInRange;
+    }
 
-	}
+    public void Interact()
+    {
+        if (!Activated())
+        {
+            //Open Note
+            noteTextUi.text = noteText;
+            noteUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            //Close Note
+            noteUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
 
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.CompareTag("Player"))
-		{
-			Debug.Log("jugador Dentro de rango");
-			playerInRange = true;
-		}
-	}
+    public bool Activated()
+    {
+		return noteUI.activeSelf;
+    }
 
-	private void OnTriggerExit(Collider other)
-	{
-		Debug.Log("jugador fuera de rango");
-		playerInRange = false;
-		noteCanvas.SetActive(false);
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("jugador Dentro de rango");
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("jugador fuera de rango");
+        playerInRange = false;
+        noteUI.SetActive(false);
+    }
 }
